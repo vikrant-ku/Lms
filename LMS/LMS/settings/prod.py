53d0d1fb94e8323df070c560fd4d66ce5f7bd8f0
vikrant-ku <vikrant.rohartech@gmail.com>
2021-05-21 12:14:59 +0530

    update requirements.txt

--- LMS/LMS/settings/prod.py
+++ LMS/LMS/settings/prod.py
@@ -2,9 +2,9 @@ from .base import *
 
 SECRET_KEY = 'django-insecure-d+qzxt3@tv+(87w53^j!ml3607^ba()cygs7f^6@fu^ov(=yx+'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["10.122.0.2"]
 
 DATABASES = {
     'default': {
